860d663bfbd7384fa9694d8a2818338ab03cd032
NguyenAndrew <anguyen29@wisc.edu>
2019-03-11 20:24:20 -0400

    Refactor calendar.js for future reusability (Plan is to remove calendar.js file, but replace it with another example)

--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -5,41 +5,43 @@ var router = express.Router();
 
 /* GET /calendar */
 router.get('/',
-  async function(req, res) {
+  async function (req, res) {
     if (!req.isAuthenticated()) {
-      // Redirect unauthenticated requests to home page
-      res.redirect('/')
-    } else {
-      let params = {
-        active: { calendar: true }
-      };
+      res.redirect('/');
+    }
 
-      // Get the access token
-      var accessToken;
+    // Try to get the access token
+    let accessToken;
+    try {
+      accessToken = await tokens.getAccessToken(req);
+    } catch (err) {
+      req.flash('error_msg', {
+        message: 'Could not get access token. Try signing out and signing in again.',
+        debug: JSON.stringify(err)
+      });
+    }
+
+    // Used to store Calendar Events
+    const params = {
+      active: { calendar: true }
+    };
+
+    // If there is an access token
+    if (accessToken && accessToken.length > 0) {
       try {
-        accessToken = await tokens.getAccessToken(req);
+        // Get the events
+        const events = await graph.getEvents(accessToken);
+        params.events = events.value;
       } catch (err) {
         req.flash('error_msg', {
-          message: 'Could not get access token. Try signing out and signing in again.',
+          message: 'Could not fetch events',
           debug: JSON.stringify(err)
         });
       }
-
-      if (accessToken && accessToken.length > 0) {
-        try {
-          // Get the events
-          var events = await graph.getEvents(accessToken);
-          params.events = events.value;
-        } catch (err) {
-          req.flash('error_msg', {
-            message: 'Could not fetch events',
-            debug: JSON.stringify(err)
-          });
-        }
-      }
-
-      res.render('calendar', params);
     }
+
+    res.render('calendar', params);
+
   }
 );
 
